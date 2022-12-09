c2eb708546a0c033eb57abe26ca76864dfe520f1
luyingqing <1696823201@qq.com>
2022-12-09 16:02:51 +0800

    first commit

---
+++ Test11.java
@@ -0,0 +1,11 @@
+package com.project.demo05;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test11 {
+    private  static  final Logger log = LoggerFactory.getLogger(Test11.class);
+    public static void main(String[] args) {
+        log.info("maven compile");
+    }
+}
